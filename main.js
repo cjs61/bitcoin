049f346a8508e66ec6be0d96f214c2224e6bc6b1
Unknown <barre.js@gmail.com>
2018-10-18 15:22:47 +0200

    Bitcoin avec Electron
    Bitcoin : https://www.youtube.com/watch?v=2RxHQoiDctI 
    accès à une librairie http pour atteindre la valeur de la monaie via l'api crypto. 
    Il faut donc l'installer en ligne de commande : npm install axios --save
    Pour plus d'info : http://cryptocompare.com/api
    40

---
+++ main.js
@@ -0,0 +1,75 @@
+const { app, BrowserWindow, Menu } = require('electron')
+const path = require('path')
+const url = require('url')
+const shell = require('electron').shell
+  
+  // Gardez une reference globale de l'objet window, si vous ne le faites pas, la fenetre sera
+  // fermee automatiquement quand l'objet JavaScript sera garbage collected.
+  let win
+  
+  function createWindow () {
+    // Créer le browser window.
+    win = new BrowserWindow({ width: 800, height: 600 })
+  
+    // et charge le index.html de l'application.
+    win.loadURL(url.format({
+      pathname: path.join(__dirname, 'src/index.html'),
+      protocol: 'file',
+      slashes: true
+    }))
+  
+    // Ouvre les DevTools.
+    win.webContents.openDevTools()
+  
+    // Émit lorsque la fenêtre est fermée.
+    win.on('closed', () => {
+      // Dé-référence l'objet window , normalement, vous stockeriez les fenêtres
+      // dans un tableau si votre application supporte le multi-fenêtre. C'est le moment
+      // où vous devez supprimer l'élément correspondant.
+      win = null
+    })
+    var menu = Menu.buildFromTemplate ([
+      {
+        label: 'Menu',
+        submenu: [
+          {label: 'Ajuster la valeur de notification'},
+          {label: 'CoinMarketCap',
+          click(){
+            shell.openExternal('http://coinmarketcap.com')
+          }
+        },
+        {type: 'separator'},
+          {label: 'Exit',
+          click(){
+            app.quit()
+          }
+            
+        },
+
+        ]
+      }]
+    )
+    Menu.setApplicationMenu(menu);
+  }
+  
+  // Cette méthode sera appelée quant Electron aura fini
+  // de s'initialiser et sera prêt à créer des fenêtres de navigation.
+  // Certaines APIs peuvent être utilisées uniquement quand cet événement est émit.
+  app.on('ready', createWindow)
+  
+  // Quitte l'application quand toutes les fenêtres sont fermées.
+  app.on('window-all-closed', () => {
+    // Sur macOS, il est commun pour une application et leur barre de menu
+    // de rester active tant que l'utilisateur ne quitte pas explicitement avec Cmd + Q
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
+  
+  app.on('activate', () => {
+    // Sur macOS, il est commun de re-créer une fenêtre de l'application quand
+    // l'icône du dock est cliquée et qu'il n'y a pas d'autres fenêtres d'ouvertes.
+    if (win === null) {
+      createWindow()
+    }
+  })
